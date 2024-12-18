8460288cf2b0ed3cf4976d5b0c8f9e648c27d191
H-SHAJI-81KSQ\Hariprasad <hariprasad_shaji_from.mhi@support.gov.sg>
2024-12-18 18:30:34 +0800

    upload file

---
+++ tagVpc.tf
@@ -0,0 +1,51 @@
+locals {
+  # flatten ensures that this local value is a flat list of objects, rather
+  # than a list of lists of objects.
+  # proceed = var.INTRANET_SUBNET_ID != "" ? false :true
+  flattenIntranetSubnet = var.BindTagIntranetSubnet == true ? flatten([
+    for subnetID in toset(var.INTRANET_SUBNET_IDS) : [
+      for tag in var.IntranetTags : {
+        subnetID  = subnetID
+        tag       = tag
+        parentID  = "//compute.googleapis.com/projects/${var.PROJECT_NUM}/regions/asia-southeast1/subnetworks/${subnetID}"
+        tag_value = google_tags_tag_value.tag_values["${tag}"].id
+      }
+    ]
+  ]) : []
+  flattenInternetSubnet = var.BindTagInternetSubnet == true ? flatten([
+    for subnetID in toset(var.INTERNET_SUBNET_IDS) : [
+      for tag in var.InternetTags : {
+        subnetID  = subnetID
+        tag       = tag
+        parentID  = "//compute.googleapis.com/projects/${var.PROJECT_NUM}/regions/asia-southeast1/subnetworks/${subnetID}"
+        tag_value = google_tags_tag_value.tag_values["${tag}"].id
+      }
+    ]
+  ]) : []
+}
+
+resource "google_tags_location_tag_binding" "IntranetBinding" {
+  # for_each = toset(var.IntranetTags)
+  for_each = {
+    for tags in local.flattenIntranetSubnet : "${tags.tag}-${tags.subnetID}" => tags if var.BindTagIntranetSubnet
+  }
+  parent    = each.value.parentID
+  tag_value = each.value.tag_value
+  location  = "asia-southeast1"
+    depends_on = [
+    google_tags_tag_value.tag_values
+  ]
+}
+
+resource "google_tags_location_tag_binding" "InternetBinding" {
+  # for_each = toset(var.IntranetTags)
+  for_each = {
+    for tags in local.flattenInternetSubnet : "${tags.tag}-${tags.subnetID}" => tags if var.BindTagInternetSubnet
+  }
+  parent    = each.value.parentID
+  tag_value = each.value.tag_value
+  location  = "asia-southeast1"
+    depends_on = [
+    google_tags_tag_value.tag_values
+  ]
+}
---
+++ variables.tf
@@ -0,0 +1,75 @@
+# gcc:origin	v2
+# gcc:team	gcci
+# ec2:ResourceTag:gcc:security:zone	gcci-intranet gcci-internet
+# cmp_compartment_type	Intranet Internet
+# type	Intranet Internet
+variable "IntranetTags" {
+  type    = list(string)
+  default = ["cmp_compartment_type-Intranet", "ec2:ResourceTag:gcc:security:zone-gcci-intranet", "type-Intranet", "gcc:team-gcci", "gcc:origin-v2"]
+}
+variable "InternetTags" {
+  type    = list(string)
+  default = ["cmp_compartment_type-Internet", "ec2:ResourceTag:gcc:security:zone-gcci-internet", "type-Internet", "gcc:team-gcci", "gcc:origin-v2"]
+}
+variable "BindTagIntranetSubnet" {
+  type    = bool
+  default = false
+}
+variable "BindTagInternetSubnet" {
+  type    = bool
+  default = false
+}
+variable "CreateTags" {
+  type    = bool
+  default = false
+}
+variable "PROJECT_NUM" {
+  type = string
+}
+variable "INTRANET_SUBNET_IDS" {
+  type = list(string)
+}
+variable "INTERNET_SUBNET_IDS" {
+  type = list(string)
+}
+variable "tags" {
+  type = map(object({
+    key         = string
+    valuesID    = list(string)
+    description = string
+  }))
+
+  default = {
+    type = {
+      key         = "type"
+      valuesID    = ["Intranet", "Internet"]
+      description = "gcci managed resource"
+    },
+    "gcc:origin" = {
+      key         = "gcc:origin"
+      valuesID    = ["v2"]
+      description = "gcci managed resource"
+    },
+    "gcc:team" = {
+      key         = "gcc:team"
+      valuesID    = ["gcci"]
+      description = "gcci managed resource"
+    },
+    "ec2:ResourceTag:gcc:security:zone" = {
+      key         = "ec2:ResourceTag:gcc:security:zone"
+      valuesID    = ["gcci-intranet", "gcci-internet"]
+      description = "gcci managed resource"
+    },
+    "cmp_compartment_type" = {
+      key         = "cmp_compartment_type"
+      valuesID    = ["Intranet", "Internet"]
+      description = "gcci managed resource"
+    }
+
+  }
+}
+
+
+variable "PROJECT_ID" {
+  type = string
+}
